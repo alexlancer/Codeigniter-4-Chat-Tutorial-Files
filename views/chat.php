9cbf2b49e52db91a2821055444806b6e19275ca6
Alex Lancer <alex@alexlancer.com>
2020-04-24 19:20:21 +0300

    Helper files

---
+++ views/chat.php
@@ -0,0 +1,30 @@
+<div class="container">
+  <div class="row">
+    <div class="col-12 mt-5 pt-3 pb-3 bg-white from-wrapper">
+      <div class="container">
+        <h3>Chat</h3>
+        <hr>
+        <div class="row">
+          <div class="col-12 col-sm-12 col-md-4 mb-3">
+            <ul id="user-list" class="list-group"></ul>
+          </div>
+          <div class="col-12 col-sm-12 col-md-8">
+            <div class="row">
+              <div class="col-12">
+                <div class="message-holder">
+                    <div id="messages" class="row"></div>
+                </div>
+                <div class="form-group">
+                 <textarea id="message-input" class="form-control" name="" rows="2"></textarea>
+                </div>
+            </div>
+              <div class="col-12">
+                <button id="send" class="btn float-right  btn-primary">Send</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</div>
